fd65a35edc044754832bafe0b14842a07246a36c
Sebastian Ospina <grinmord@gmail.com>
2023-09-18 09:34:06 +0100

    Errores Resueltos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
-import {  NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
-import { ItemComponent } from './pages/item/item.component';
-import { AppModule } from './AppModule';
 
 const app_routes: Routes = [
   { path: '', component: PortafolioComponent, pathMatch: 'full' },
   { path: 'about', component: AboutComponent, pathMatch: 'full' },
   { path: 'portafolioitem', component: PortafolioComponent, pathMatch: 'full' },
   { path: '**', pathMatch: 'full', redirectTo: '' }
-  ];
+];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot( app_routes)
+    RouterModule.forRoot(app_routes)
+  ],
+  exports: [
+    RouterModule
   ]
-});
-
-// export class AppModule{}
+})
+export class AppRoutingModule {}
